30246da03fa993496c3703841f70e236ea41f4e4
hannon <hannonq@yahoo.com.br>
2016-02-01 02:41:03 -0200

    fudeu login user

--- src/main/java/com/bcc481/services/CurrentUserDetailsService.java
+++ src/main/java/com/bcc481/services/CurrentUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.bcc481.services;
 
-import com.bcc481.models.CurrentUser;
 import com.bcc481.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/com/bcc481/services/CurrentUserService.java
+++ src/main/java/com/bcc481/services/CurrentUserService.java
@@ -1,8 +1,6 @@
 package com.bcc481.services;
 
 
-import com.bcc481.models.CurrentUser;
-
 public interface CurrentUserService {
 
     boolean canAccessUser(CurrentUser currentUser, Long userId);
